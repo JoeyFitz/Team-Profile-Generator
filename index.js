04d8f6063c9594d03f8c1dc475b86e7bd654a1e6
Joe Fitzgerald <jfitzgerald@veritone.com>
2021-08-15 15:35:09 -0700

    node modules + updated test scripts

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 const inquirer = require ('inquirer');
+const fs = require('fs');
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: "What is the team member's name?",
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: "What is the team member's ID?", 
+            name: 'ID',
+        },
+        {
+            type: 'input',
+            message: "What is the team member's email address?",
+            name: 'email',
+        }
+    ])
+}
+    const generateHTML = (answers) =>
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+        <p class="lead">I am from ${answers.location}.</p>
+        <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+        <ul class="list-group">
+        <li class="list-group-item">My GitHub username is ${answers.github}</li>
+        <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+        </ul>
+    </div>
+    </div>
+</body>
+</html>`;
+  
+// Bonus using writeFileAsync as a promise
+const init = () => {
+promptUser()
+    .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
+    .then(() => console.log('Successfully wrote to index.html'))
+    .catch((err) => console.error(err));
+};
+
+init();
