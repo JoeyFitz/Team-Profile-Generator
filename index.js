84dca2a81f2ea27b208a60b4766f9d9aa57251a3
Joe Fitzgerald <jfitzgerald@veritone.com>
2021-08-15 23:12:30 -0700

    updated inquirer prompt Qs

--- index.js
+++ index.js
@@ -4,59 +4,116 @@ const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 
-const commonQuestions = [
+const managerQuestions = [
     {
         type: 'input',
-        message: "What is the team member's name?",
-        name: 'name',
+        message: "What is the team manager's name?",
+        name: 'managerName',
     },
     {
         type: 'input',
-        message: "What is the team member's ID?", 
-        name: 'ID',
+        message: "What is the team manager's ID?", 
+        name: 'managerID',
     },
     {
         type: 'input',
-        message: "What is the team member's email address?",
-        name: 'email',
+        message: "What is the team manager's email?",
+        name: 'managerEmail',
+    },
+    {
+        type: 'input',
+        message: "What is the team manager's officer number?",
+        name: 'managerNumber',
     },
     {
         type: 'checkbox',
-        message: "What is the team member's role?",
-        name: 'role',
-        choices: ['Manager', 'Engineer', 'Intern'],
+        message: "Which type of team member would you like to add?",
+        name: 'memberType',
+        choices: ['Engineer', 'Intern', "I don't want to add any more team members"]
+    }
+];
+    
+const engineerQuestions = [
+    {
+        type: 'input',
+        message: "What is your engineer's name?",
+        name: 'engineerName',
     },
+    {
+        type: 'input',
+        message: "What is your engineer's ID?", 
+        name: 'engineerID',
+    },
+    {
+        type: 'input',
+        message: "What is your engineer's email?",
+        name: 'engineerEmail',
+    },
+    {
+        type: 'input',
+        message: "What is your engineer's GitHub username?",
+        name: 'githubUsername',
+    },
+    {
+        type: 'checkbox',
+        message: "Which type of team member would you like to add?",
+        name: 'memberType',
+        choices: ['Engineer', 'Intern', "I don't want to add any more team members"]
+    }
 ];
 
-const internQuestion = 
+const internQuestions = [
     {
         type: 'input',
-        message: "Which school does this team member attend?",
-        name: 'school',
-    };
-
-const engineerQuestion =
+        message: "What is your intern's name?",
+        name: 'internName',
+    },
     {
         type: 'input',
-        message: "What is this team member's GitHub name?",
-        name: 'GitHub',
-    };
-
-const managerQuestion =
+        message: "What is your intern's ID?", 
+        name: 'internID',
+    },
     {
         type: 'input',
-        message: "What is this team member's office number?",
-        name: 'number',
-    };
+        message: "What is your intern's email?",
+        name: 'internEmail',
+    },
+    {
+        type: 'input',
+        message: "What is your intern's school?",
+        name: 'internSchool',
+    },
+    {
+        type: 'checkbox',
+        message: "Which type of team member would you like to add?",
+        name: 'memberType',
+        choices: ['Engineer', 'Intern', "I don't want to add any more team members"]
+    }
+];
 
 const promptUser = () => {
     inquirer
-        .prompt(commonQuestions);
+        .prompt(initialQuestions)
         .then((response) => {
-
+            if(response.role === 'Manager') {
+                inquirer
+                    .prompt(managerQuestion)
+                    .then(managerResponse)
+            }
+            else if(response.role === 'Engineer') {
+                inquirer
+                    .prompt(engineerQuestion)
+            }
+            else if(response.role === 'Intern') {
+                inquirer
+                    .prompt(internQuestion)
+            }
         })
-}
-    const generateHTML = (answers) =>
+
+
+
+
+const generateHTML = (responses) =>
 `<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -68,12 +125,12 @@ const promptUser = () => {
 <body>
     <div class="jumbotron jumbotron-fluid">
     <div class="container">
-        <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-        <p class="lead">I am from ${answers.location}.</p>
+        <h1 class="display-4">Hi! My name is ${responses.name}</h1>
+        <p class="lead">I am from ${responses.location}.</p>
         <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
         <ul class="list-group">
-        <li class="list-group-item">My GitHub username is ${answers.github}</li>
-        <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+        <li class="list-group-item">My GitHub username is ${responses.github}</li>
+        <li class="list-group-item">LinkedIn: ${responses.linkedin}</li>
         </ul>
     </div>
     </div>
@@ -82,10 +139,11 @@ const promptUser = () => {
   
 // Bonus using writeFileAsync as a promise
 const init = () => {
-promptUser()
-    .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
-    .then(() => console.log('Successfully wrote to index.html'))
-    .catch((err) => console.error(err));
+    console.log("Please build your team");
+    promptUser()
+        .then((responses) => writeFileAsync('index.html', generateHTML(responses)))
+        .then(() => console.log('Successfully wrote to index.html'))
+        .catch((err) => console.error(err));
 };
 
 init();
