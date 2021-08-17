664e6e6d3e094d7af05e8279fb3fcb9498677501
Joe Fitzgerald <jfitzgerald@veritone.com>
2021-08-16 22:32:38 -0700

    revised inquirer path and added html generation functions

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 
+const team = [];
+
 const managerQuestions = [
     {
         type: 'input',
@@ -25,12 +27,6 @@ const managerQuestions = [
         message: "What is the team manager's officer number?",
         name: 'managerNumber',
     },
-    {
-        type: 'checkbox',
-        message: "Which type of team member would you like to add?",
-        name: 'memberType',
-        choices: ['Engineer', 'Intern', "I don't want to add any more team members"]
-    }
 ];
     
 const engineerQuestions = [
@@ -54,12 +50,6 @@ const engineerQuestions = [
         message: "What is your engineer's GitHub username?",
         name: 'githubUsername',
     },
-    {
-        type: 'checkbox',
-        message: "Which type of team member would you like to add?",
-        name: 'memberType',
-        choices: ['Engineer', 'Intern', "I don't want to add any more team members"]
-    }
 ];
 
 const internQuestions = [
@@ -83,67 +73,160 @@ const internQuestions = [
         message: "What is your intern's school?",
         name: 'internSchool',
     },
-    {
-        type: 'checkbox',
-        message: "Which type of team member would you like to add?",
-        name: 'memberType',
-        choices: ['Engineer', 'Intern', "I don't want to add any more team members"]
-    }
 ];
 
-const promptUser = () => {
+const continueQuestion = 
+    {
+        type: 'list',
+        message: "Which type of team member would you like to add?",
+        name: 'continue',
+        choices: ['Engineer', 'Intern', "I don't want to add any more team members"]
+    } 
+
+
+const initialPrompt = () => {
     inquirer
-        .prompt(initialQuestions)
-        .then((response) => {
-            if(response.role === 'Manager') {
-                inquirer
-                    .prompt(managerQuestion)
-                    .then(managerResponse)
+        .prompt(managerQuestions)
+        .then((managerResponse) => {
+            let teammate = new Manager(managerResponse.managerName, managerResponse.managerID, managerResponse.managerEmail, managerResponse.managerNumber);
+            console.log(teammate);
+            team.push(teammate);
+            beginningHTML();
+            continuePrompt();
+        })
+    }
+
+const continuePrompt = () => {
+    inquirer
+        .prompt(continueQuestion)
+        .then((continueResponse) => {
+            if(continueResponse.continue === "I don't want to add any more team members"){
+                endingHTML();
+                return
             }
-            else if(response.role === 'Engineer') {
-                inquirer
-                    .prompt(engineerQuestion)
+            if(continueResponse.continue === 'Engineer'){
+                engineerPrompt();
             }
-            else if(response.role === 'Intern') {
-                inquirer
-                    .prompt(internQuestion)
+            if(continueResponse.continue === 'Intern'){
+                internPrompt();
             }
         })
+}
 
+const engineerPrompt = () => {
+    inquirer    
+        .prompt(engineerQuestions)
+        .then((engineerResponse) => {
+            let teammate = new Engineer(engineerResponse.engineerName, engineerResponse.engineerID, engineerResponse.engineerEmail, engineerResponse.githubUsername)
+            console.log(teammate);
+            team.push(teammate);
+            additionalHTML(teammate);
+            continueQuestion();
+        })
+}
 
+const internPrompt = () => {
+    inquirer    
+        .prompt(internQuestions)
+        .then((internResponse) => {
+            let teammate = new Intern(internResponse.internName, internResponse.internID, internResponse.internEmail, internResponse.internSchool)
+            console.log(teammate);
+            team.push(teammate);
+            additionalHTML(teammate);
+            continueQuestion();
+        })
+}
 
+const beginningHTML = () => {
+    let html = 
+    `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-dark mb-5">
+            <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
+        </nav>
+        <div class="container">
+            <div class="row">`;
+    fs.writeFile('./dist/index.html', html, (err) => err ? console.log(err): console.log('Success!')) 
+}
 
-const generateHTML = (responses) =>
-`<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <title>Document</title>
-</head>
-<body>
-    <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-        <h1 class="display-4">Hi! My name is ${responses.name}</h1>
-        <p class="lead">I am from ${responses.location}.</p>
-        <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-        <ul class="list-group">
-        <li class="list-group-item">My GitHub username is ${responses.github}</li>
-        <li class="list-group-item">LinkedIn: ${responses.linkedin}</li>
-        </ul>
-    </div>
-    </div>
-</body>
-</html>`;
+const additionalHTML = (teammate) => {
+    return new Promise((resolve, reject) => {
+        const name = teammate.getName();
+        const role = teammate.getRole();
+        const id = teammate.getID();
+        const email = teammate.getEmail();
+        let newHTML = "";
+        if(role === 'Engineer') {
+            const github = teammate.getGithub();
+            newHTML = 
+            `<div class="col-6">
+                <div class="card mx-auto mb-3" style="width: 18rem">
+                    <h5 class="card-header">${name}<br /><br />Engineer</h5>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">ID: ${id}</li>
+                        <li class="list-group-item">Email Address: ${email}</li>
+                        <li class="list-group-item">GitHub: ${gitHub}</li>
+                    </ul>
+                </div>
+            </div>`;
+        }
+        else if (role === 'Intern') {
+            const school = teammate.getSchool();
+            newHTML =
+            `<div class="col-6">
+                <div class="card mx-auto mb-3" style="width: 18rem">
+                    <h5 class="card-header">${name}<br /><br />Intern</h5>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">ID: ${id}</li>
+                        <li class="list-group-item">Email Address: ${email}</li>
+                        <li class="list-group-item">School: ${school}</li>
+                    </ul>
+                </div>
+            </div>`;
+        }
+        else {
+            const officeNumber = teammate.getOfficeNumber();
+            newHTML = 
+            `<div class="col-6">
+                <div class="card mx-auto mb-3" style="width: 18rem">
+                    <h5 class="card-header">${name}<br /><br />Manager</h5>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">ID: ${id}</li>
+                        <li class="list-group-item">Email Address: ${email}</li>
+                        <li class="list-group-item">Office Phone: ${officeNumber}</li>
+                    </ul>
+                </div>
+            </div>`
+        }
+        fs.appendFile('./dist/index.html', newHTML, (err) => {
+            if (err) {
+                return reject(err);
+            };
+            return resolve();
+        });
+    })
+}
+
+const endingHTML = () => {
+    let html = 
+    `       </div>
+        </div>
+    </body>
+    </html>`;
+    fs.appendFile('./dist/index.html', html, (err) => err ? console.log(err): console.log('Success!'))
+}
   
-// Bonus using writeFileAsync as a promise
 const init = () => {
-    console.log("Please build your team");
-    promptUser()
-        .then((responses) => writeFileAsync('index.html', generateHTML(responses)))
-        .then(() => console.log('Successfully wrote to index.html'))
-        .catch((err) => console.error(err));
+    console.log("Please build your team.");
+    initialPrompt();
 };
 
 init();
